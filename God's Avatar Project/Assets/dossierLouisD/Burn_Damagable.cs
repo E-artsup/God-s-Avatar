90d188c0326d5f5f49011ae2957010ce5ad75898
Codabs <devillelouis01@gmail.com>
2022-02-10 21:13:13 +0100

    Momie + Momie Boss

--- God's Avatar Project/Assets/dossierLouisD/Burn_Damagable.cs
+++ God's Avatar Project/Assets/dossierLouisD/Burn_Damagable.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Burn_Damagable : MonoBehaviour
 {
     //Zones Des Variables
     public GameObject Laser_ref;
     public GameObject ArmePlayer_ref;
+    public GameObject Artéphact_ref;
     public float HP;
     public float DamageParAttaque;
 
@@ -16,35 +18,48 @@ public class Burn_Damagable : MonoBehaviour
     public bool Momie_Burn;
     MeshRenderer Body;
     public Material DamageMaterial;
+    NavMeshAgent agent;
+    public bool Momie_Boss;
+
+    public Material BurnMomie;
+    public Material BurnHat1;
+    public Material BurnHat2;
+    public Material[] MomieBoss;
 
     private void OnEnable()
     {
         Laser = Laser_ref.GetComponent<Collider>();
         ArmePlayer = ArmePlayer_ref.GetComponent<Collider>();
         Body = gameObject.GetComponentInChildren<MeshRenderer>();
+        agent = gameObject.GetComponent<NavMeshAgent>();
     }
     private void OnCollisionEnter(Collision col)
     {
         if (col.gameObject == Laser_ref)
         {
             Momie_Burn = true;
+            agent.speed = 2;
+            SkinnedMeshRenderer skinnedMeshRenderer = GetComponent<SkinnedMeshRenderer>();
+            skinnedMeshRenderer.material = BurnMomie;
+            if (gameObject.name == "Momie_Boss")
+            {
+                MeshRenderer meshRenderer = GetComponentInChildren<MeshRenderer>();
+                meshRenderer.material = BurnMomie;
+                meshRenderer.materials = MomieBoss;
+            }
         }
         if (col.gameObject == ArmePlayer_ref & Momie_Burn == true)
         {
             HP = HP - DamageParAttaque;
-            Material StockageTemporaire = Body.material;
-            for (float i = 0; i <= 100;) 
-            {
-                i = i + Time.deltaTime;
-                Body.material = DamageMaterial;
-            }
-            Body.material = StockageTemporaire;
             if (HP <= 0)
                 Die();
             if (gameObject.name == "Momie_Boss")
             {
                 if (HP <= 50)
-                    DamageParAttaque = 15;
+                {
+                    Momie_Boss = true;
+                }
+
             }
         }
 
@@ -53,5 +68,9 @@ public class Burn_Damagable : MonoBehaviour
     private void Die()
     {
         Destroy(gameObject);
+        if (gameObject.name == "Momie_Boss")
+        {
+            Instantiate(Artéphact_ref);
+        }
     }
 }
